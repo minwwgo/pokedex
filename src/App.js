60060979da49a6e835ec607b7b4072dbdd80df20
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-05-20 14:17:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Logo from './Logo';
 import BestPokemon from './BestPokemon'
 import CaughtPokemon from './CaughtPokemon'
+import Footer from './Footer'
 
 function App() {
   const abilities = ["Anticipation", "Adaptability", "Run-Away"];
   const newDate = new Date().toLocaleDateString()
   return (
     <div>
+      <Footer/>
       <Logo  appName="Pokedex"/>
       <BestPokemon  abilities={abilities}/>
       <CaughtPokemon newDate={newDate}/>
