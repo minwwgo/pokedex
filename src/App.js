1396a5823d23bc68a242e94f2e36760a3e0494e7
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-05-20 14:11:14 +0100

    header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Logo from './Logo';
 import BestPokemon from './BestPokemon'
 import CaughtPokemon from './CaughtPokemon'
+import Header from './Header'
+
 
 function App() {
   const abilities = ["Anticipation", "Adaptability", "Run-Away"];
   const newDate = new Date().toLocaleDateString()
   return (
     <div>
+      <Header/>
       <Logo  appName="Pokedex"/>
       <BestPokemon  abilities={abilities}/>
       <CaughtPokemon newDate={newDate}/>
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Header = () => {
+  return(
+    <h1> hello world </h1>
+  )
+}
+
+export default Header;
