b720980c18b599a8503b15c84152f83374300451
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-05-20 15:56:14 +0100

    delete welcome

--- src/Logo.js
+++ src/Logo.js
@@ -6,7 +6,7 @@ function Logo(props) {
   return (
     <div>
       <header>
-        <h1>Welcome to the {props.appName}</h1>
+        <h1> {props.appName}</h1>
         <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex" />
       </header>
     </div>
